15c6dc28611e78274511dd25e0b08c309e15c4f1
Bisrat Yonatan <bisratyonatan1224@gmail>
2026-05-25 10:43:22 +0300

    fixed package.json

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,7 @@ async function bootstrap() {
   const serverConfig = config.get<ServerConfigInterface>('server');
   const logger = new Logger("bootstrap");
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "http://localhost:5173",
-  });
+  app.enableCors();
   const port = process.env.PORT || serverConfig.port;
   await app.listen(process.env.PORT ?? port);
   logger.log(`Application listening on port ${port}`);
