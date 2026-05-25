c6df0589f41a779e9cd5413bfa7b67a437b1606f
Bisrat Yonatan <bisratyonatan1224@gmail>
2026-05-25 10:20:46 +0300

    database and backend deployment

--- src/Config/typeorm.config.ts
+++ src/Config/typeorm.config.ts
@@ -1,20 +1,23 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 import config from 'config'
-import { DBConfigInterface } from 'config/ConfigInterfaces/dbConfig.interface';
+import { DBConfigInterface } from 'config/ConfigInterfaces/dbConfig.interface'
+import 'dotenv/config'
 
-const dbConfig = config.get<DBConfigInterface>('db');
-export const typeOrmConfig : TypeOrmModuleOptions = {
-    type: dbConfig.type,
-    host: process.env.RDS_HOSTNAME ||  dbConfig.host,
-    port: process.env.RDS_PORT
-    ? parseInt(process.env.RDS_PORT, 10)
-    : dbConfig.port,
-    username: process.env.RDS_USERNAME ||  dbConfig.username,
-    password: process.env.RDS_PASSWORD ||  dbConfig.password,
-    database: process.env.RDS_DB_NAME || dbConfig.database,
-    entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-    synchronize: process.env.TYPEORM_SYNC ?
-    process.env.TYPEORM_SYNC === 'true' : 
-    dbConfig.synchronize,
-    // autoLoadEntities: true
+const dbConfig = config.get<DBConfigInterface>('db')
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+
+    
+    url: process.env.DATABASE_URL,
+
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+
+    synchronize: process.env.TYPEORM_SYNC
+        ? process.env.TYPEORM_SYNC === 'true'
+        : dbConfig.synchronize,
+
+    ssl: {
+        rejectUnauthorized: false,
+    },
 }
